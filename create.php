d7799ea243a89ff7775e8375f0d5a9857126cdc1
fain07 <yotion@live.com>
2014-07-27 19:24:46 +0800

    dbconnect

---
+++ create.php
@@ -0,0 +1,10 @@
+﻿
+<?php
+require_once "dbconnect.php";
+$getallsql="select * from `call`;";
+$resultt=mysql_query($getallsql);
+echo "<table><tr><td>Name</td><td>Phone</td></tr>";
+while($roww=mysql_fetch_array($resultt)){
+        echo "<tr><td>".utf8_decode($roww['name'])."</td><td>".$roww['phone']."</td></tr>";
+}
+echo "</table>";
--- dbconnect.php
+++ dbconnect.php
@@ -1,9 +1,9 @@
 ﻿<?php
 
 //连接数据库questionnaire
-$db_host='localhost';
-$db_user='root';
-$db_password='';
-$db_name='mycall';
+$db_host='mysql://$OPENSHIFT_MYSQL_DB_HOST:$OPENSHIFT_MYSQL_DB_PORT/';
+$db_user='admins535gLD';
+$db_password='aTcej7Tj5PdF';
+$db_name='php';
 $connect=mysql_connect($db_host,$db_user,$db_password);
-mysql_select_db($db_name,$co
+mysql_select_db($db_name,$connect);
