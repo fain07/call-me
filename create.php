c53c0f3491ee888eeb270ab9e6584fe68f2c72b1
fain07 <yotion@live.com>
2014-07-27 19:25:44 +0800

    test

--- create.php
+++ create.php
@@ -1,5 +1,6 @@
-﻿
+﻿hehe
 <?php
+/*
 require_once "dbconnect.php";
 $getallsql="select * from `call`;";
 $resultt=mysql_query($getallsql);
@@ -7,4 +8,4 @@ echo "<table><tr><td>Name</td><td>Phone</td></tr>";
 while($roww=mysql_fetch_array($resultt)){
         echo "<tr><td>".utf8_decode($roww['name'])."</td><td>".$roww['phone']."</td></tr>";
 }
-echo "</table>";
+echo "</table>";*/
